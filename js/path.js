f4b74597d7d0982c28bdf02a69cceb3bd515ce50
李琳 <ll@fjreading.com>
2019-06-05 11:31:34 +0800

    添加

--- js/path.js
+++ js/path.js
@@ -1,10 +1,10 @@
 // 生产环境地址
-// const basepath = "https://share.fjreading.com"
+const basepath = "https://www.fjreading.com"
 // 测试环境地址
 // const basepath = "https://sharedev.fjreading.com"
 // qa环境地址
-// const basepath = "http://shareqa.fjreading.com"
-// pre环境地址
-// const basepath = "http://sharepre.fjreading.com"
+// const basepath = "http://qa.web.fenjiread.com" 
+
 // 跨域代理地址
-const basepath = "/dev"
+// const basepath = "/dev"
+
